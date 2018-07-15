1078f2b792b7cf67f84deebd8d37a7fa34901b2e
ecrucru <ecrucru@users.noreply.github.com>
2018-07-15 15:32:38 +0200

    Update scripts for npm

--- scripts/build_npm.bat
+++ scripts/build_npm.bat
@@ -6,9 +6,12 @@ copy package.json node_modules\anticrux
 
 copy LICENSE      node_modules\anticrux
 copy README.md    node_modules\anticrux
+copy yuidoc.json  node_modules\anticrux
 copy *.css        node_modules\anticrux
 copy *.js         node_modules\anticrux
 copy *.html       node_modules\anticrux
+copy *.url        node_modules\anticrux
+copy images\*.bmp node_modules\anticrux\images
 copy images\*.gif node_modules\anticrux\images
 copy images\*.png node_modules\anticrux\images
 
--- scripts/build_npm.sh
+++ scripts/build_npm.sh
@@ -8,8 +8,11 @@ cp package.json node_modules/anticrux
 
 cp LICENSE      node_modules/anticrux
 cp README.md    node_modules/anticrux
+cp yuidoc.json  node_modules/anticrux
 cp *.css        node_modules/anticrux
 cp *.js         node_modules/anticrux
 cp *.html       node_modules/anticrux
+cp *.url        node_modules/anticrux
+cp images/*.bmp node_modules/anticrux/images
 cp images/*.gif node_modules/anticrux/images
 cp images/*.png node_modules/anticrux/images
